6aa98bf6b83b1d7f1b384c30d6775f41e66ed743
ghoshgrishm <ghoshgrishm@gmail.com>
2025-06-10 11:33:02 +0530

    change is admissions

--- check_admissions.php
+++ check_admissions.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['patient_name'])) {
         $name = mysqli_real_escape_string($conn, $_POST['patient_name']);
         
-        $sql = "SELECT a.admission_id, p.patient_name AS patient_name, d.dept_name AS department_name, b.bed_number, a.admission_date, a.predict_discharge
+        $sql = "SELECT a.admission_id, p.patient_name AS patient_name, d.dept_name AS department_name, b.bed_number, a.patient_condition, a.admission_date, a.predict_discharge
                 FROM admission a
                 JOIN patient p ON a.patient_id = p.patient_id
                 JOIN department d ON a.department_id = d.department_id
